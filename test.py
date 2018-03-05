9b8cda5eb5b3661ac20bae7acf70ed1ae4db930d
Paul <paulsmith.contactinfo@gmail.com>
2018-03-05 14:27:28 -0800

    Test

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-# shebang line if neccessay for unix system
-
-# Andrew De La Fuente & Paul Smith
+#!/usr/bin/env python3
+""""FTP Protocols"""
+__author__ = "Andrew De La Fuente and Paul Smith"
+__copyright__ = "Copyright (C) 2018 Andrew De La Fuente and Paul Smith"
+__license__ = "Public Domain"
+__version__ = "1.0"
 
 """ some description for a file """
 print ("Hello World")
